00dd20ea45c80a2a78e35eabf91806871e54cf1f
anpark <zhuoan@baidu.com>
2020-03-28 20:12:10 +0800

    update travis python info

--- test/lr_test.sh
+++ test/lr_test.sh
@@ -2,14 +2,14 @@
 
 root_dir=$(dirname $(readlink -f $0))/..
 
-sed -i "s#fluid_bin=.*#fluid_bin=python#g" conf/var_sys.conf
+sed -i "s#fluid_bin=.*#fluid_bin=python3#g" conf/var_sys.conf
 #sed -i "s|cuda_lib_path|#cuda_lib_path|g" conf/var_sys.conf
 
 #train
 sh run.sh -c conf/linear_regression/linear_regression.local.conf
 
 if [ $? -ne 0 ];then
-    echo "[FATAL] $(date) lr train failure." >&2
+    echo "$(date) lr_test train failure." >&2
     exit 1
 fi
 
