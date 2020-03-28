4fceebe46bf9d906efb9f0f484a79117ca82fb1d
anpark <zhuoan@baidu.com>
2020-03-28 18:50:58 +0800

    update travis python info

--- test/lr_test.sh
+++ test/lr_test.sh
@@ -9,6 +9,7 @@ sed -i "s#fluid_bin=.*#fluid_bin=python#g" conf/var_sys.conf
 sh run.sh -c conf/linear_regression/linear_regression.local.conf
 
 if [ $? -ne 0 ];then
+    echo "[FATAL] $(date) lr train failure." >&2
     exit 1
 fi
 
